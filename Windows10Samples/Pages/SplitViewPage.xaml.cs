0b6946e524d388e938210c637008725da781a2cf
Daniel Hindrikes <daniel@hindrikes.se>
2015-07-30 15:29:25 +0200

    SplitView sample

--- Windows10Samples/Pages/SplitViewPage.xaml.cs
+++ Windows10Samples/Pages/SplitViewPage.xaml.cs
@@ -12,25 +12,24 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows10Samples.Pages;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Windows10Samples
+namespace Windows10Samples.Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SplitViewPage : Page
     {
-        public MainPage()
+        public SplitViewPage()
         {
             this.InitializeComponent();
         }
 
-        private void SplitView_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(SplitViewPage));
+            Split.IsPaneOpen = !Split.IsPaneOpen;
         }
     }
 }
